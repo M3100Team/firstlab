f5b1590a5f479d9b3dad08d4a4fa64724a6147cc
goosescout <michael.gurevich.02@mail.ru>
2021-11-26 23:57:42 +0300

    Тесты логина

--- frontend/cypress/integration/homepage/login.spec.js
+++ frontend/cypress/integration/homepage/login.spec.js
@@ -3,5 +3,16 @@ describe("Login", () => {
     cy.visit("/");
 
     cy.contains("Номер ИСУ");
+    cy.contains("Пароль");
+  });
+
+  it("can log in", () => {
+    cy.visit("/");
+
+    cy.get("[data_cy=number-input]").type("334790");
+    cy.get("[data_cy=password-input]").type("123456");
+    cy.get("[data_cy=login-form]").submit();
+
+    cy.contains("Линейная алгерба I");
   });
 });
